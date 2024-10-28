42c2e46266805760c4b5e509e3ecf54b75c4595f
churcchill <churcchill@Churchill>
2024-10-28 16:15:12 +0530

    update

---
+++ com.demowebkshop/src/test/java/Selenium_Pratices/Github.java
@@ -0,0 +1,14 @@
+package Selenium_Pratices;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.model.Report;
+
+public class Github {
+
+	@Test
+	public void tesecase() {
+		Reporter.log("This Class is Githud Class",true);
+	}
+}
