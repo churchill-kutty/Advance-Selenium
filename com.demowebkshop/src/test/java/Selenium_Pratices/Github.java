2091088181bcc5895ae0413a8bb397ebf1a21ceb
churcchill <churcchill@Churchill>
2024-10-28 16:22:06 +0530

    Update

--- com.demowebkshop/src/test/java/Selenium_Pratices/Github.java
+++ com.demowebkshop/src/test/java/Selenium_Pratices/Github.java
@@ -1,15 +1,12 @@
 package Selenium_Pratices;
 
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-
-
 public class Github {
-
 	@Test
-	public void tesecase() {
-		Reporter.log("This Class is Githud Class",true);
-		Reporter.log("This Class is Githud Class",true);
+	public void testcasde() {
+		
+		
 	}
+
 }
